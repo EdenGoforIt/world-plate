658a1e53492aca91f55062eb406e584be0959556
Eden Park <edengoforit@gmail.com>
2025-09-30 17:52:06 +1300

    feat: layout added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import Toast from 'react-native-toast-message';
+import "../global.css";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { preloadRecipeCache } from '@/utils/recipeCache';
