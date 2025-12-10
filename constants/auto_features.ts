b38f75abe88d2fad5ddee25b2b180541860649de
Eden Park <edengoforit@gmail.com>
2025-12-11 10:00:00 +1300

    feat: add auto feature #17 (2025-12-11)

--- constants/auto_features.ts
+++ constants/auto_features.ts
@@ -17,3 +17,4 @@ export const AUTO_FEATURE_13 = "2025-12-09 10:00:00";
 export const AUTO_FEATURE_14 = "2025-12-09 16:00:00";
 export const AUTO_FEATURE_15 = "2025-12-10 10:00:00";
 export const AUTO_FEATURE_16 = "2025-12-10 16:00:00";
+export const AUTO_FEATURE_17 = "2025-12-11 10:00:00";
